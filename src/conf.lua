571ce2c4e98a451121ca5416405e4bf1545469a1
Loucee <54353633+Loucee@users.noreply.github.com>
2021-05-18 20:14:07 +0200

    Create conf.lua

---
+++ src/conf.lua
@@ -0,0 +1,11 @@
+
+function love.conf(t)
+	t.version = "11.3"
+	t.console = false
+
+	t.window.resizable = false
+    t.window.width = 800
+    t.window.height = 600
+
+	t.window.title = "Spacewar!"
+end
